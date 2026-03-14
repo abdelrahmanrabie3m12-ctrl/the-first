731af3fb20c7a200487b9b4c416e6036af925bab
Abdelrahman Rabie <abdelrahman.rabie3.m12@gmail.com>
2026-03-14 13:54:28 +0300

    Add files via upload

---
+++ aboda2.js
@@ -0,0 +1,8 @@
+alert("welcome")
+let name = prompt("enter your name")
+let age = prompt("enter your age")
+
+document.write("Hello"+ name)
+document.write("<br>")
+document.write("He is"+ age + " years old")
+
